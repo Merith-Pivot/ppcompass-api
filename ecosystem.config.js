30029de32cd313703a8733fa3ccb64adb46eb2da
Merith-Pivot <merith@pivotandco.com>
2024-01-26 16:15:22 +0100

    fix: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'ppc-api',
+      name: 'ppc-api', 
       script: 'uvicorn',
-      args: 'app.main:app --reload',
+      args: 'app.main:app --reload', 
+      autorestart: true,
       watch: true,
+      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
       },
     },
   ],
